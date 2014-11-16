40beb31cd76a66e288660dab443b729686f29dbf
nuclearmistake <nuclearmistake@gmail.com>
2014-11-16 15:17:25 -0500

    Revert "assert stock JDK version for build flag BONE_STOCK"
    Not worth the risk. It's early.
    
    This reverts commit b0dac0435a9c564efbc26d98d0eddfacec118584.
    
    Change-Id: I9c1249b7db784946b4b359bd56e54634344459e6

--- core/main.mk
+++ core/main.mk
@@ -169,9 +169,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/initializing.html)
 $(info ************************************************************)
-ifeq ($(BONE_STOCK),true)
-  $(error stop)
-endif
+$(error stop)
 endif
 
 # Check for the current JDK.
@@ -195,9 +193,7 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-ifeq ($(BONE_STOCK),true)
-  $(error stop)
-endif
+$(error stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -208,9 +204,7 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-ifeq ($(BONE_STOCK),true)
-  $(error stop)
-endif
+$(error stop)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -226,9 +220,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-ifeq ($(BONE_STOCK),true)
-  $(error stop)
-endif
+$(error stop)
 endif
 
 
