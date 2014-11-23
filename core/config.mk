e13b68625f6b4b93d23c1d4f71d955708244934e
MartinRo <martin@rodemann.org>
2014-11-23 10:52:20 +0100

    We are vanir...resistance is futile
    Change-Id: I1dfee7c72632cd4260897ddf2e8be6e8391e0527

--- core/config.mk
+++ core/config.mk
@@ -621,10 +621,8 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
-ifneq ($(CM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/cm/sepolicy/sepolicy.mk)
-endif
+$(eval include vendor/vanir/sepolicy/sepolicy.mk)
 
 include $(BUILD_SYSTEM)/dumpvar.mk
