776002a76f702eaff627b97e10e5c505dc01f85d
DHOMD <DHO@dho.im>
2015-02-16 16:09:56 -0500

    vanir_config: Fix a ton of builds using fstrict aliasing
    edited for exodus by AlmightyMegadeth00
    
    Change-Id: I7e07ec3b8cf291258e272ac853521018eef9e3c6
    Signed-off-by: DHOMD <DHO@dho.im>

--- core/vanir_config.mk
+++ core/vanir_config.mk
@@ -118,68 +118,75 @@ endif
 # fstrict-aliasing. Thumb is defaulted off for AOSP. Use VANIR_SPECIAL_CASE_MODULES to
 # temporarily disable fstrict-aliasing locally in modules we dont care about or until the
 # error it contains is properly fixed.
+#
+# Style points will be assessed for tagging modules with their path for future fixing
 ifeq ($(USE_FSTRICT_FLAGS),true)
   VANIR_FNO_STRICT_ALIASING_MODULES := \
-    libziparchive-host \
-	logd \
-	mdnsd \
-	libziparchive \
-	libdiskconfig \
-	libtwrpmtp \
-	libfusetwrp \
-	libguitwrp \
-	busybox \
-	static_busybox \
-	libuclibcrpc \
-	ping \
-	ping6 \
-	libjavacore \
-	libfdlibm \
-	libvariablespeed \
-	librtp_jni \
-	libldnhncr \
-	libwilhelm \
-	libstlport \
-	libstlport_static \
-	libutils \
-	dnsmasq \
-	libwebviewchromium \
-	libwebviewchromium_loader \
-	libwebviewchromium_plat_support \
-	content_content_renderer_gyp \
-	net_net_gyp \
-	third_party_WebKit_Source_modules_modules_gyp \
-	third_party_WebKit_Source_platform_blink_platform_gyp \
-	third_party_WebKit_Source_core_webcore_remaining_gyp \
-	third_party_angle_src_translator_lib_gyp \
-	third_party_WebKit_Source_core_webcore_generated_gyp \
-	libft2 \
-	libjni_jpegstream \
-	libjni_filtershow_filters \
-	libjni_jpegutil \
-	gatt_testtool \
-	bluetooth.default \
-	sensors.$(TARGET_BOOTLOADER_BOARD_NAME) \
-	libnvvisualizer \
-	libnfc-nci \
-	libssh \
-	ssh \
-	libOmxVdec \
-	libOmxVenc \
-    mm-vdec-omx-test \
-    libpdfiumcore \
-    libpdfium \
-    libstagefright_soft_h264dec \
+    audio.primary.msm8960 \
+    audio.primary.msm8974 \
+    audio_policy.msm8610 \
+    AudioHardwareALSA \
+    bluetooth.default \
+    busybox \
+    content_content_renderer_gyp \
+    dnsmasq \
+    gatt_testtool \
+    libdiskconfig \
+    libfdlibm \
+    libft2 \
+    libfusetwrp \
+    libguitwrp \
+    libjavacore \
+    libjni_filtershow_filters \
+    libjni_jpegstream \
+    libjni_jpegutil \
+    libldnhncr \
     libmusicbundle \
+    libnfc-nci \
+    libnvvisualizer \
+    libOmxVdec \
+    libOmxVenc \
+    libpdfium \
+    libpdfiumcore \
+    libqcomvisualizer \
     libreverb \
-    libqcomvisualizer
+    librtp_jni \
+    libssh \
+    libstagefright_soft_h264dec \
+    libstlport \
+    libstlport_static \
+    libtwrpmtp \
+    libuclibcrpc \
+    libutils \
+    libvariablespeed \
+    libvisualizer \
+    libwebviewchromium \
+    libwebviewchromium_loader \
+    libwebviewchromium_plat_support \
+    libwilhelm \
+    libziparchive-host \
+    libziparchive \
+    logd \
+    mdnsd \
+    mm-vdec-omx-test \
+    net_net_gyp \
+    ping6 \
+    ping \
+    sensors.$(TARGET_BOOTLOADER_BOARD_NAME) \
+    ssh \
+    static_busybox \
+    third_party_angle_src_translator_lib_gyp \
+    third_party_WebKit_Source_core_webcore_generated_gyp \
+    third_party_WebKit_Source_core_webcore_remaining_gyp \
+    third_party_WebKit_Source_modules_modules_gyp \
+    third_party_WebKit_Source_platform_blink_platform_gyp
 
 # external/ffmpeg
   VANIR_FNO_STRICT_ALIASING_MODULES += \
-	libavcodec \
-	libavutil \
-	libavformat \
-	libswscale
+    libavcodec \
+    libavformat \
+    libavutil \
+    libswscale
 
   FSTRICT_FLAGS := \
           -fstrict-aliasing \
