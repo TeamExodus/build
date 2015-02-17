fd6a1200458bb8fb51d01f3a2c10cf30e9ebef8e
Dave Kessler <activethrasher00@gmail.com>
2015-02-16 23:18:54 -0500

    armv8-a: Add arch_variant_cflags for denver64
    Change-Id: Ic40f53922f0e56f599e266f85a648b10a0bb0966

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,1 +1,10 @@
-arch_variant_cflags :=
+# Configuration for Linux on ARM.
+# Generating binaries for the ARMv8-a architecture
+#
+ifneq (,$(filter denver64,$(TARGET_CPU_VARIANT)))
+
+arch_variant_cflags := \
+    -mcpu=cortex-a57 \
+    -march=armv8-a
+
+endif
