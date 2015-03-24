f34df057ed8b642535cf77e1be8ef32b95d09cda
Dave Kessler <activethrasher00@gmail.com>
2015-03-24 01:01:51 -0400

    armv7-a-neon: refine arch flags
    previously we were only setting mcpu.  we can assign mtune here also.
    
    Change-Id: I2aabecc34aa1aa0fd0ffa22470ec707d98e20d1c

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -10,20 +10,24 @@ ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 	# TODO: krait is not a cortex-a15, we set the variant to cortex-a15 so that
 	#       hardware divide operations are generated. This should be removed and a
 	#       krait CPU variant added to GCC/clang.
-	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a15 -mtune=cortex-a15 -mfpu=neon-vfpv4
 
 	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
 	# don't advertise.
 	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a9 -mtune=cortex-a9 -mfpu=neon
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_cflags := -mcpu=cortex-a8 -mtune=cortext-a8 -mfpu=neon
+
+	# not sure if this is still necessary, but we were assigning to all chipset arches.
+	arch_variant_ldflags := \
+	-Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_cflags := -mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=neon-vfpv4
 else
 	arch_variant_cflags := -march=armv7-a -mfpu=neon
 endif
@@ -33,6 +37,3 @@ endif
 
 arch_variant_cflags += \
     -mfloat-abi=softfp
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
