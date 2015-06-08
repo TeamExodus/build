02cc5ad518189f239be61bf194e2b805c81b6bf5
Dave Kessler <activethrasher00@gmail.com>
2015-06-08 19:46:35 -0400

    Revert "armv8-a: add configuration for denver64"
    This reverts commit 09f798c0909477b26f0b5f6d87fd555f194c35bf.
    
    Change-Id: Ia9c3f77457f2e2dea7143510ff0357908c10f1a3

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,11 +1,1 @@
-# Configuration for Linux on ARM.
-# Generating binaries for the ARMv8-a architecture
-#
-ifneq (,$(filter denver64,$(TARGET_CPU_VARIANT)))
-
-arch_variant_cflags := \
-    -mcpu=cortex-a57 \
-    -mtune=cortex-a57 \
-    -march=armv8-a 
-
-endif
+arch_variant_cflags :=
