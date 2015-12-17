9cdf796f76330fe84ad38186b163091c270c7211
Joe Maples <frap129@gmail.com>
2015-12-17 07:52:31 -0500

    core: Don't build any tests
    Change-Id: I2911dd62ef082ac6c3c77aa335fb71ec966ab6d4

--- core/main.mk
+++ core/main.mk
@@ -1011,13 +1011,6 @@ target-java-tests : java-target-tests
 target-native-tests : native-target-tests
 tests : host-tests target-tests
 
-# To catch more build breakage, check build tests modules in eng and userdebug builds.
-ifneq ($(TARGET_BUILD_PDK),true)
-ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
-droidcore : target-tests host-tests
-endif
-endif
-
 .PHONY: lintall
 
 ifneq (,$(filter samplecode, $(MAKECMDGOALS)))
