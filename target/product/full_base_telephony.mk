fc093923449a888926b69ca74851092331b3dcc4
Alex Naidis <alex.naidis@linux.com>
2016-11-11 21:19:38 +0100

    full_base_telephony: add back handheld capabilities
    These must be present on all devices supporting
    telephony functions. Otherwise the result is major
    internal platform breakage.
    
    This was previously removed by mistake.
    
    Change-Id: Ia17b0060b59e1654127ce43064e44cd15019aed0
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -23,10 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=true
 
-ifneq ($(TARGET_SUPPORTS_WEARABLES), true)
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
